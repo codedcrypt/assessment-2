51cd8cdd7e0856e25e2efdeefb2c9b7180f37585
codedshell <sharafaabidemi@outlook.com>
2023-06-07 13:51:44 +0100

    first commit

--- src/component/input.jsx
+++ src/component/input.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const input = () => {
   return (
-    <div className='yes' > <input type="text" /></div>
+    <div className='yes' > <input type="text" placeholder='Enter Your Text Here......' /></div>
   )
 }
 
