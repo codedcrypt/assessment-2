0f3abc9a883234ea658a0385ce48d1bd352ed2db
codedcrypt <127953576+codedcrypt@users.noreply.github.com>
2023-06-14 15:20:24 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React,{ useState} from 'react'
+import React,{useState} from 'react'
 import Output from './component/output'
 import Input from './component/input'
-import "./App.css"
+import './App.css'
 
 function App() {
   const [username, setUsername] = useState("james")
